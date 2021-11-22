5fae9e462f1b8a3a0d7a876649a47dc3d8f1155b
ruben <>
2021-11-22 19:53:39 +0100

    Creado proyecto Java

---
+++  EjerciciosProgramacionJava/src/ejerciciosprogramacionjava/EjerciciosProgramacionJava.java
@@ -0,0 +1,21 @@
+/*
+ * Proyecto EjerciciosProgramacionJava - Archivo EjerciciosProgramacionJava.java - Compañía DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+package ejerciciosprogramacionjava;
+
+/**
+ *
+ * @author Rubén Pérez <rpcausias@gmail.com>
+ */
+public class EjerciciosProgramacionJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
